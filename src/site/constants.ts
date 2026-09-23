d2d0fcc1d54985a9b1ae3888b81685953248240d
Tiberius Dourado <tiberiusthefifth@gmail.com>
2026-09-23 11:26:39 -0300

    Install from a prebuilt release branch

--- src/site/constants.ts
+++ src/site/constants.ts
@@ -1,2 +1,2 @@
 export const REPO_URL = 'https://github.com/TiberiusNemesis/sonner-sv';
-export const INSTALL_COMMAND = 'npm install github:TiberiusNemesis/sonner-sv';
+export const INSTALL_COMMAND = 'npm install github:TiberiusNemesis/sonner-sv#release';
